9b9cf5bead91ba2c58913fcf9a03fd2a3745bcc7
C-NET <jfranzani@c-net.com.ar>
2014-06-23 17:41:50 -0300

    .
    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,60 +9,10 @@ MYAPP.run = (function() {
         transition: "slide"
     });
 
-
-
-    //Test para quitar de memoria 
-    //window.localStorage.removeItem('eula-flag');
-    
-    /*Check EULA flag*/
-    var eula = window.localStorage.getItem('eula-flag');
-
-    if (eula == null || !eula) 
-        MYAPP.app.navigate("#eula");
-    else
-        MYAPP.app.navigate("#ListCategoriesView.html");
+    MYAPP.app.navigate("#ListCategoriesView.html");
     
     window.plugins.emailComposer = new EmailComposer();
 });
-
-
-
-MYAPP.acceptEULA = function () {
-
-    var nombre = $("#txtNombre").val();
-    var email = $("#txtEmail").val();
-    var tel = $("#txtTelefono").val();
-
-    if (nombre == null || nombre == '')
-    {
-        alert ('Debe completar el Nombre')
-        return false;
-    }
-    if (email == null || email == '') {
-        alert('Debe completar su direcci\u00F3n de correo electr\u00F3nico')
-        return false;
-    }
-    if (tel == null || tel == '') {
-        alert('Debe ingresar su tel\u00E9fono')
-        return false;
-    }
-    //Guardar información de usuario
-    window.localStorage.setItem('nombre', nombre);
-    window.localStorage.setItem('email', email);
-    window.localStorage.setItem('telefono', tel);
-
-    window.localStorage.setItem('eula-flag', true);
-    window.localStorage.setItem('eula-accept', true);
-    MYAPP.app.navigate("#ListCategoriesView.html");
-};
-
-    
-MYAPP.refuseEULA = function () {
-    //navigator.app.exitApp();
-    window.localStorage.setItem('eula-flag', true);
-    window.localStorage.setItem('eula-accept', false);
-    MYAPP.app.navigate("#ListCategoriesView.html");
-};
 
 // this is called when the initial view shows. it prevents the flash
 // of unstyled content (FOUC)
@@ -139,16 +89,12 @@ MYAPP.showIndexList = new kendo.data.DataSource({
     group: { field: "categoryCode" }
 });
 
-
 //FUNCIONALIDAD BOTÓN COMPARTIR
 MYAPP.sendMail = function (title, subtitle, encoded64) {
-
     var now = new Date();
     //Concatena día, fecha, hora, segundos
-    var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("-"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(""), AddZero(now.getSeconds())].join("");
-   
+    var strDateTime = [[AddZero(now.getDate()), AddZero(now.getMonth() + 1), now.getFullYear()].join("-"), [AddZero(now.getHours()), AddZero(now.getMinutes())].join(""), AddZero(now.getSeconds())].join("");   
     window.plugins.emailComposer.showEmailComposer(title, 'Adjunto se encuentra una p\u00e1gina de un ensayo cl\u00ednico: ' + title + '\n' + subtitle, null, null, null, false, null, [['Articulo_' + strDateTime + '.html', encoded64]]);
-
 };
 //Pad given value to the left with "0"
 function AddZero(num) {
@@ -158,12 +104,9 @@ function AddZero(num) {
 
 //BUSCA EL INDICE DEL ARTICULO DE LA PALABRA ENCONTRADA
 MYAPP.find = function (key, categories) {
-    var idx = MYAPP.idx;
- 
+    var idx = MYAPP.idx; 
     key = key.toLowerCase();
-
     var match = [];
-
     // Recorre el índice
     for (var i = 0; i < idx.length; i++)
     {
@@ -183,7 +126,6 @@ MYAPP.find = function (key, categories) {
             }
         }
     }
-
     var data = [];
     //Filtra los artículos por categoría
     for (var i = 0; i < match.length; i++) {
@@ -191,16 +133,13 @@ MYAPP.find = function (key, categories) {
         if ((art.categoryCode & categories) != 0)
             data.push(art);
     }
-
     return data;
 };
 
 MYAPP.cambiarArticulo = function (indice, direccion) {
-    if (indice < 0) return false;
-
+    if (indice < 0 || indice > 510) return false;
     MYAPP.app.navigate("\#abstracts/" + MYAPP.src[indice].article, 'slide:'+direccion);
 };
-
 
 //Obtiene el nombre de la categoría mediante el código.
 MYAPP.getCategoryName = function (categoryCode) {
@@ -227,22 +166,10 @@ MYAPP.CheckAll = function (e) {
 
 }
 
-MYAPP.navigateToArticle = function (article) {
-    /*Chequeo si acepta compartir informacion con Novartis*/
-    var eulaAceptado = window.localStorage.getItem('eula-accept');
-    if (eulaAceptado == 'false')
-        MYAPP.app.navigate("#eula");
-    else
-        MYAPP.app.navigate(article);
+MYAPP.navigateToArticle = function (article)
+{
+    MYAPP.app.navigate(article);
 }
-
-MYAPP.hideFooter = function (e) {
-    $(".km-tabstrip").hide();
-};
-
-MYAPP.showFooter = function (e) {
-    $(".km-tabstrip").show(); 
-};
 
 MYAPP.scrollTop = function (e) {
     e.preventDefault();
@@ -250,48 +177,10 @@ MYAPP.scrollTop = function (e) {
     scroller.reset(); 
 };
 
-MYAPP.GuardarEstadisticasAbstract = function (articulo) {
-    //Se guardarán las estadísticas de uso
-}
+MYAPP.hideFooter = function (e) {
+    $(".km-tabstrip").hide();
+};
 
-
-//CODIGO QUE PUEDO LLEGAR A NECESITAR: (BORRAR LUEGO)
-
-//window.plugins.telephoneNumber.get(function (result) {
-//    alert('re' + 'sult =' + result);
-//}, function (error) {
-//    alert('error = ' + error.code);
-//});
-
-//COMENTADO LLAMADA AJAX  A WEB SERVICE
-//MYAPP.call(
-//    "Check",
-//    { code: code },
-//    function(result) {
-//        if (result == "OK") {
-//            window.localStorage.setItem('eula-flag', true);
-//            MYAPP.app.navigate("#home");
-//        } else {
-//            /* CODIGO INCORRECTO */
-//        }
-//    },
-//    function(result, error) {
-//        /* ERROR SERVICIO NO DISPONIBLE */
-//    }
-//);
-
-
-//MYAPP.call = function (operation, data, successFn, errorFn) {
-//    $.ajax({
-//        url: "http://localhost:3355/Auth.svc/" + operation,
-//        dataType: "jsonp",
-//        data: data,
-//        crossDomain: true,
-//        type: "POST",
-//        username: null,
-//        password: null,
-//        timeout: 10000, //10 segundos de TO
-//        success: successFn,
-//        error: errorFn
-//    });
-//};
+MYAPP.showFooter = function (e) {
+    $(".km-tabstrip").show();
+};
