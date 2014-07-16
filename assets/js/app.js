dc857624c9c64a72700f4b4e0c0171392291107b
C-NET <jfranzani@c-net.com.ar>
2014-07-16 16:07:24 -0300

    .
    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,7 @@ MYAPP.search = (function (e) {
         var indice = $("#buttongroup").data("kendoMobileButtonGroup").selectedIndex;
         //Si esta chequeada la categoria
         if (indice == 0) {
+            MYAPP.abstracts.sort({ field: "title", dir: "asc" });
             MYAPP.abstracts.group({ field: "category" });
             return true;
         }
